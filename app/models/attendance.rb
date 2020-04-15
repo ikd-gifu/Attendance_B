2a7f821ccfe3d3bc62e6cfd56148e6ff9db53bc4
ikd-gifu <oki19ki@gmail.com>
2020-04-15 14:01:04 +0000

    20200415 date

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -7,7 +7,9 @@ class Attendance < ApplicationRecord
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
   
+  # 退勤時間が存在しない場合、出勤時間は無効
   validate :started_at_is_invalid_without_a_finished_at
+  
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
 
@@ -16,7 +18,9 @@ class Attendance < ApplicationRecord
   end
   
   def started_at_is_invalid_without_a_finished_at
-    errors.add(:finished_at, "が必要です") if finished_at.blank? && started_at.present?
+    # unless Date.current
+      errors.add(:finished_at, "が必要です") if finished_at.blank? && started_at.present?
+    # end
   end
 
   def started_at_than_finished_at_fast_if_invalid
